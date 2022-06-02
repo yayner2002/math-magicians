693f5788ddc742dec376a4f39b30cac0d3ff3832
yayner <yayner2002@gmail.com>
2022-06-02 18:16:08 +0300

    Refactor with functional component using useState hooks

--- src/App.js
+++ src/App.js
@@ -1,33 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Calculator from './components/Calculator';
 import './components/Calculator.css';
-import calculate from './logic/calculate';
 
-// eslint-disable-next-line react/prefer-stateless-function
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      total: 0,
-      next: null,
-      operation: null,
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick = (element) => {
-    const buttonName = element.target.id;
-    this.setState((state) => calculate(state, buttonName));
-  };
-
-  render() {
-    const { total, next, operation } = this.state;
-    return (
-      <div className="App">
-        <Calculator total={total} next={next} operation={operation} onBtnClick={this.handleClick} />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <Calculator />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,149 +1,156 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import calculate from '../logic/calculate';
 
-// eslint-disable-next-line react/prefer-stateless-function
-class Calculator extends Component {
-  render() {
-    const {
-      // eslint-disable-next-line react/prop-types
-      total, operation, next, onBtnClick,
-    } = this.props;
-    return (
-      <div className="container">
-        <div className="result-container">
-          <span>{total}</span>
-          <span>{operation}</span>
-          <span>{next}</span>
-        </div>
-        <div className="first-row">
-          <button type="button" onClick={onBtnClick} id="AC">
-            AC
-          </button>
-          <button type="button" onClick={onBtnClick} id="+/-">
-            +/-
-          </button>
-          <button type="button" onClick={onBtnClick} id="%">
-            %
-          </button>
-          <button type="button" onClick={onBtnClick} id="÷">
-            ÷
-          </button>
-        </div>
-        <div className="second-row">
-          <button
-            type="button"
-            onClick={onBtnClick}
-            id="7"
-          >
-            7
-          </button>
-          <button
-            type="button"
-            onClick={onBtnClick}
-            id="8"
-          >
-            8
-          </button>
-          <button
-            type="button"
-            onClick={onBtnClick}
-            id="9"
-          >
-            9
-          </button>
-          <button
-            type="button"
-            onClick={onBtnClick}
-            id="x"
-          >
-            x
-          </button>
-        </div>
-        <div className="third-row">
-          <button
-            type="button"
-            onClick={onBtnClick}
-            id="4"
-          >
-            4
-          </button>
-          <button
-            type="button"
-            onClick={onBtnClick}
-            id="5"
-          >
-            5
-          </button>
-          <button
-            type="button"
-            onClick={onBtnClick}
-            id="6"
-          >
-            6
-          </button>
-          <button
-            type="button"
-            onClick={onBtnClick}
-            id="-"
-          >
-            -
-          </button>
-        </div>
-        <div className="fourth-row">
-          <button
-            type="button"
-            onClick={onBtnClick}
-            id="1"
-          >
-            1
-          </button>
-          <button
-            type="button"
-            onClick={onBtnClick}
-            id="2"
-          >
-            2
-          </button>
-          <button
-            type="button"
-            onClick={onBtnClick}
-            id="3"
-          >
-            3
-          </button>
-          <button
-            type="button"
-            onClick={onBtnClick}
-            id="+"
-          >
-            +
-          </button>
-        </div>
-        <div className="fifth-row">
-          <button
-            type="button"
-            onClick={onBtnClick}
-            id="0"
-          >
-            0
-          </button>
-          <button
-            type="button"
-            onClick={onBtnClick}
-            id="."
-          >
-            .
-          </button>
-          <button
-            type="button"
-            onClick={onBtnClick}
-            id="="
-          >
-            =
-          </button>
-        </div>
-      </div>
-    );
+function Calculator() {
+  const [state, setState] = useState({
+    total: 0,
+    next: null,
+    operation: null,
+  });
+  const { total } = state;
+  const { next } = state;
+  const { operation } = state;
+
+  function handleClick(element) {
+    const buttonName = element.target.id;
+    setState((state) => calculate(state, buttonName));
   }
+  return (
+    <div className="container">
+      <div className="result-container">
+        <span>{total}</span>
+        <span>{operation}</span>
+        <span>{next}</span>
+      </div>
+      <div className="first-row">
+        <button type="button" onClick={handleClick} id="AC">
+          AC
+        </button>
+        <button type="button" onClick={handleClick} id="+/-">
+          +/-
+        </button>
+        <button type="button" onClick={handleClick} id="%">
+          %
+        </button>
+        <button type="button" onClick={handleClick} id="÷">
+          ÷
+        </button>
+      </div>
+      <div className="second-row">
+        <button
+          type="button"
+          onClick={handleClick}
+          id="7"
+        >
+          7
+        </button>
+        <button
+          type="button"
+          onClick={handleClick}
+          id="8"
+        >
+          8
+        </button>
+        <button
+          type="button"
+          onClick={handleClick}
+          id="9"
+        >
+          9
+        </button>
+        <button
+          type="button"
+          onClick={handleClick}
+          id="x"
+        >
+          x
+        </button>
+      </div>
+      <div className="third-row">
+        <button
+          type="button"
+          onClick={handleClick}
+          id="4"
+        >
+          4
+        </button>
+        <button
+          type="button"
+          onClick={handleClick}
+          id="5"
+        >
+          5
+        </button>
+        <button
+          type="button"
+          onClick={handleClick}
+          id="6"
+        >
+          6
+        </button>
+        <button
+          type="button"
+          onClick={handleClick}
+          id="-"
+        >
+          -
+        </button>
+      </div>
+      <div className="fourth-row">
+        <button
+          type="button"
+          onClick={handleClick}
+          id="1"
+        >
+          1
+        </button>
+        <button
+          type="button"
+          onClick={handleClick}
+          id="2"
+        >
+          2
+        </button>
+        <button
+          type="button"
+          onClick={handleClick}
+          id="3"
+        >
+          3
+        </button>
+        <button
+          type="button"
+          onClick={handleClick}
+          id="+"
+        >
+          +
+        </button>
+      </div>
+      <div className="fifth-row">
+        <button
+          type="button"
+          onClick={handleClick}
+          id="0"
+        >
+          0
+        </button>
+        <button
+          type="button"
+          onClick={handleClick}
+          id="."
+        >
+          .
+        </button>
+        <button
+          type="button"
+          onClick={handleClick}
+          id="="
+        >
+          =
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Calculator;
